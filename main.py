57173c580ffcd882e1c683cfbeb99a77e06c29a9
Cristian <garcia.cristian@outlook.com>
2022-05-24 02:20:40 -0500

    done

--- main.py
+++ main.py
@@ -6,3 +6,9 @@ def pairs_sum_to_target(list1, list2, target):
             if list1[i] + list2[j] == target:
                 sums.append([i,j])
     return sums
+
+list1 = [1, -2, 4, 5, 9]
+list2 = [4, 2, -4, -4, 0]
+target = 5
+
+print(pairs_sum_to_target(list1,list2, target))
